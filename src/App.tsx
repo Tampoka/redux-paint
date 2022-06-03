f7cb0c8e6878f128b8455851530aba02d6ef0fa7
Tampoka <goal.thanks@gmail.com>
2022-06-03 12:53:24 +0900

    Add startDrawing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
-import {useRef} from 'react';
+import React, {useRef} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {currentStrokeSelector} from './selectors';
+import {beginStroke} from './actions';
 
 function App() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
-    const startDrawing = () => { }
-    const endDrawing = () => { }
-    const draw = () => { }
+    const currentStroke = useSelector(currentStrokeSelector)
+    const dispatch = useDispatch()
+
+    const startDrawing = ({nativeEvent}: React.MouseEvent<HTMLCanvasElement>) => {
+        const {offsetX, offsetY} = nativeEvent
+        dispatch(beginStroke(offsetX, offsetY))
+    }
+
+    const endDrawing = () => {
+    }
+    const draw = () => {
+    }
     return (
         <canvas
             onMouseDown={startDrawing}
