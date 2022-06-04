cce8f41b8b2b796670ae77e85bb2e8de3c02a48a
Tampoka <goal.thanks@gmail.com>
2022-06-04 22:16:48 +0900

    Add modals slice.

--- src/store.ts
+++ src/store.ts
@@ -3,13 +3,15 @@ import logger from 'redux-logger';
 import historyIndex from './modules/historyIndex/slice';
 import strokes from './modules/strokes/slice';
 import currentStroke from './modules/currentStroke/slice';
+import {modalVisible} from './modules/modals/slice';
 
 const middleware = [...getDefaultMiddleware(), logger]
 export const store = configureStore({
     reducer: {
-        historyIndex: historyIndex,
-        currentStroke: currentStroke,
-        strokes: strokes
+        historyIndex,
+        currentStroke,
+        strokes,
+        modalVisible
     },
     middleware
 
