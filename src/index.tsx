bf1c9e04d63fbd8025b37bf4b976a88a01d4f6bd
Tampoka <goal.thanks@gmail.com>
2022-08-02 19:24:31 +0900

    Fix CanvasProvider.

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import {store} from './store';
+import {CanvasProvider} from './CanvasContext';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     // <React.StrictMode>
-        <Provider store={store}>
+    <Provider store={store}>
+        <CanvasProvider>
             <App/>
-        </Provider>
+        </CanvasProvider>
+    </Provider>
     // </React.StrictMode>
 );
